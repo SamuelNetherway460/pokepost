ee00fdd90e11cb27efc549ae65d6079f499dcde8
Samuel Netherway <samuelnetherwaygit@gmail.com>
2020-11-03 22:21:29 +0000

    Added custom comment dummy data for test purposes and call to comment factory.

---
+++ database/seeds/CommentTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Comment;
+
+class CommentTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $comment1 = new Comment;
+        $comment1->content = "Yes, there great!";
+        $comment1->date_commented = "2020-11-01";
+        $comment1->time_commented = "17:55:44";
+        $comment1->user_id = 2;
+        $comment1->post_id = 1;
+        $comment1->save();
+
+        factory(App\Comment::class, 100)->create();
+    }
+}
