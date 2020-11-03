5f9e52341390bd4ce6c4993ae396f4788ad7146e
Samuel Netherway <samuelnetherwaygit@gmail.com>
2020-11-03 22:18:24 +0000

    Updated User factory with additional faker fields

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,9 +19,14 @@ use Illuminate\Support\Str;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
+        'title' => $faker->title,
+        'firstname' => $faker->firstName,
+        'lastname' => $faker->lastName,
+        'username' => $faker->userName,
+        'phone_number' => $faker->phoneNumber,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
+        'address' => $faker->address,
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
     ];
