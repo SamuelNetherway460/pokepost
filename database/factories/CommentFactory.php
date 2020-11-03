c4c3a2df06ff56bd1f5b13722276607ab885ce3d
Samuel Netherway <samuelnetherwaygit@gmail.com>
2020-11-03 22:18:54 +0000

    Added faker fields for Comment factory

---
+++ database/factories/CommentFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Comment;
+use Faker\Generator as Faker;
+
+$factory->define(Comment::class, function (Faker $faker) {
+    return [
+        'content' => $faker->realText(200),
+        'date_commented' => $faker->date('Y-m-d', 'now'),
+        'time_commented' => $faker->time('H:i:s', 'now'),
+        'user_id'=>App\User::inRandomOrder()->first()->id,
+        'post_id'=>App\Post::inRandomOrder()->first()->id,
+    ];
+});
