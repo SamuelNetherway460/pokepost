936f330d2651f19cefde0bae45b457888e618456
Samuel Netherway <samuelnetherwaygit@gmail.com>
2020-11-06 21:41:52 +0000

    Minor Refactoring

--- database/seeds/PostTableSeeder.php
+++ database/seeds/PostTableSeeder.php
@@ -14,8 +14,9 @@ class PostTableSeeder extends Seeder
     {
         $post1 = new Post;
         $post1->title = "Hello Everyone!";
-        $post1->content = "Hi Everyone. My name is Bob Ross and I'm new to SPost. Who likes my paintings?";
-        $post1->image = "";
+        $post1->content = "Hi Everyone. My name is Bob Ross and I'm new to SPost. 
+            Who likes my paintings?";
+        $post1->image = "https://lorempixel.com/640/480/?31610";
         $post1->user_id = 1;
         $post1->save();
 
