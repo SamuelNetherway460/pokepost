e667fca38538e1382fe70c9d6af822bdb3419512
Samuel Netherway <samuelnetherwaygit@gmail.com>
2020-12-11 21:58:22 +0000

    Added a basic layout and posts view which extends the layout.

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>PokePost - @yield('title')</title>
+    </head>
+<body>
+    <h1>PokePost - @yield('title')</h1>
+    <div>
+        @yield('content')
+    </div>
+</body>
+</html>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('title', 'Posts')
+
+@section('content')
+    <p>Posts about pokemon will be added here</p>
+@endsection
