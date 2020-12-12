3fae6d3f48970d049dfeebc8e9bff65c1fc78157
Samuel Netherway <samuelnetherwaygit@gmail.com>
2020-12-12 16:08:31 +0000

    Added timestamped posts. Added list of comments to each post.

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,7 +6,7 @@
     <p>All Posts:</p>
     <ul>
         @foreach ($posts as $post)
-            <li><a href="{{ route('posts.show', $post) }}">{{ !empty($post->user->profile) ? $post->user->profile->firstname:'' }}</a></li>
+            <li><a href="{{ route('posts.show', $post) }}">{{ $post->user->profile->firstname }} {{ $post->user->profile->lastname }} posted at {{ $post->created_at }}</a></li>
         @endforeach
     </ul>
     {{ $posts->links() }}
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,6 +7,15 @@
         <li>Title: {{$post->title}}</li>
         <li>Content: {{$post->content}}</li>
         <li>Image URL: <a href="{{$post->image}}">Post Image</a></li>
+        <li>Posted: {{$post->created_at}}</li>
+        @if ($post->updated_at > $post->created_at)
+            <li>Changed: {{$post->updated_at}}</li>
+        @endif
     </ul>
-    <p>Commets:</p>
+    <p>Comments:</p>
+    @php $comments = $post->comments @endphp
+    @foreach ($comments as $comment)
+        <b>{{$comment->user->profile->firstname}} {{$comment->user->profile->lastname}} at {{$comment->created_at}}</b>
+        <p>{{$comment->content}}</p>
+    @endforeach
 @endsection
