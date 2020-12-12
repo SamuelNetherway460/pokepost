3d8ea7c4fd79564a92d75a61c05809c0c17d21d1
Samuel Netherway <samuelnetherwaygit@gmail.com>
2020-12-12 20:08:47 +0000

    Create post field data doesn't disappear after failed validation

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -5,9 +5,12 @@
 @section('content')
     <form method="POST" action="{{ route('posts.store') }}">
         @csrf
-        <p>Title: <input type="text" name="title" ></p>
-        <p>Content: <input type="text" name="content" ></p>
-        <p>Image: <input type="image" name="image" ></p>
+        <p>Title: <input type="text" name="title"
+            value="{{ old('title') }}"></p>
+        <p>Content: <input type="text" name="content"
+            value="{{ old('content') }}"></p>
+        <p>Image: <input type="image" name="image"
+            value="{{ old('image') }}"></p>
         <input type="submit" value="Submit">
         <a href="{{ route('posts.index') }}">Cancel</a>
     </form>
