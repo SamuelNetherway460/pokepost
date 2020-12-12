4ad57ab174ffe9b396b7da61cbd6560df06333cd
Samuel Netherway <samuelnetherwaygit@gmail.com>
2020-12-12 16:37:20 +0000

    Added link to create post on the main feed.

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,7 +3,8 @@
 @section('title', 'Posts')
 
 @section('content')
-    <p>All Posts:</p>
+    <a href="{{ route('posts.create') }}">Create Post</a>
+    <h1>All Posts:</h1>
     <ul>
         @foreach ($posts as $post)
             <li><a href="{{ route('posts.show', $post) }}">{{ $post->user->profile->firstname }} {{ $post->user->profile->lastname }} posted at {{ $post->created_at }}</a></li>
