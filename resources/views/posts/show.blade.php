b471789ed147a2471e131c19b121128ea88a9709
Samuel Netherway <samuelnetherwaygit@gmail.com>
2020-12-12 16:12:56 +0000

    Formatting data for a post.

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,16 +3,16 @@
 @section('title', 'Post Detail')
 
 @section('content')
+    <h2>{{ $post->user->profile->firstname }} {{ $post->user->profile->lastname }} at {{ $post->created_at }}</h2>
     <ul>
         <li>Title: {{$post->title}}</li>
         <li>Content: {{$post->content}}</li>
         <li>Image URL: <a href="{{$post->image}}">Post Image</a></li>
-        <li>Posted: {{$post->created_at}}</li>
         @if ($post->updated_at > $post->created_at)
             <li>Changed: {{$post->updated_at}}</li>
         @endif
     </ul>
-    <p>Comments:</p>
+    <h2>Comments:</h2>
     @php $comments = $post->comments @endphp
     @foreach ($comments as $comment)
         <b>{{$comment->user->profile->firstname}} {{$comment->user->profile->lastname}} at {{$comment->created_at}}</b>
