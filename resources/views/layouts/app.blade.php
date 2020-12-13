6752261496a7d2e51eb1b3e2286a418882787002
Samuel Netherway <samuelnetherwaygit@gmail.com>
2020-12-13 00:33:07 +0000

    Testing git branch

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="en">
     <head>
-        <title>PokePost - @yield('title')</title>
+        <title>PokePost This One- @yield('title')</title>
     </head>
 <body>
     <h1>PokePost - @yield('title')</h1>
