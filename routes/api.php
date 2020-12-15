a8ca6a8b82b13769f8aae9caa31a3f4471f7bc28
Samuel Netherway <samuelnetherwaygit@gmail.com>
2020-12-15 15:10:53 +0000

    Added comment routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('comments', 'CommentController@apiIndex')->name('api.comments.index');
+
+Route::post('comments', 'CommentController@apiStore')->name('api.comments.store');
