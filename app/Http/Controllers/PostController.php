0d506d9efa86cc56877af8ddb3506f9d399e12ae
Samuel Netherway <samuelnetherwaygit@gmail.com>
2020-12-18 13:08:18 +0000

    Posts now ordered from most to least recent

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -17,7 +17,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::with('user')->simplePaginate(15);
+        $posts = Post::with('user')->orderBy('updated_at', 'desc')->simplePaginate(15);
         return view('posts.index', ['posts' => $posts]);
     }
 
