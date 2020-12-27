e7c5a14ec13ae486b0f9deb879108ea7fae040ad
Samuel Netherway <samuelnetherwaygit@gmail.com>
2020-12-27 22:18:36 +0000

    [POK-3/POK-144] Fixed various syntax errors in Pokemon constructor.

--- app/Pokemon/Pokemon.php
+++ app/Pokemon/Pokemon.php
@@ -4,25 +4,35 @@ namespace App\Pokemon;
 
 class Pokemon
 {
-    public $name = 'No Info';
-    public $baseExperience = 'No Info';
-    public $height = 'No Info';
-    public $weight = 'No Info';
-    public $abilities = [];
-    public $moves = [];
-    public $hp = 'No Info';
-    public $attack = 'No Info';
-    public $specialAttack = 'No Info';
-    public $speed = 'No Info';
-    public $types = [];
-    public $defence = 'No Info';
-    public $specialDefence = 'No Info';
+    public $name;
+    public $baseExperience;
+    public $height;
+    public $weight;
+    public $abilities;
+    public $moves;
+    public $hp;
+    public $attack;
+    public $specialAttack;
+    public $speed;
+    public $types;
+    public $defence;
+    public $specialDefence;
 
     public function __construct($name, $abilities, $baseExperience, $height, $weight,
         $moves, $hp, $attack, $specialAttack, $defence, $specialDefence, $speed, $types)
     {
         $this->name = $name;
+        $this->abilities = $abilities;
         $this->baseExperience = $baseExperience;
         $this->height = $height;
+        $this->weight = $weight;
+        $this->moves = $moves;
+        $this->hp = $hp;
+        $this->attack = $attack;
+        $this->specialAttack = $specialAttack;
+        $this->defence = $defence;
+        $this->specialDefence = $specialDefence;
+        $this->speed = $speed;
+        $this->types = $types;
     }
 }
