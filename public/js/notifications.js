cdc59826912bc915b41237121e96cc3e1bf4f90c
Samuel Netherway <samuelnetherwaygit@gmail.com>
2020-12-30 23:11:17 +0000

    [POK-18] Removed redundant notification partial views and .js files.

--- public/js/notifications.js
+++
@@ -1,3 +0,0 @@
-function markNotificationAsRead() {
-    $.get('/markAsRead');
-}
--- resources/views/layouts/partials/notification_types/post_deleted.blade.php
+++
@@ -1,1 +0,0 @@
-<a href="">Your post "{{ $notification->data['post']['title'] }}" was deleted by {{ $notification->data['user']['name'] }}</a>
