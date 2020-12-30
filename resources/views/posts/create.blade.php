1f65e089f514eb18ebc03297717497c37f101ee0
Samuel Netherway <samuelnetherwaygit@gmail.com>
2020-12-30 23:20:14 +0000

    [POK-125] Added in line errors for the create post view.

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -12,14 +12,29 @@
                 <h2 class="pb-2 mb-0">New Post</h2>
             </div>
             <div class="form-group input-group-lg mb-3">
-                <textarea name="title" class="form-control" aria-label="With textarea" aria-describedby="inputGroup-sizing-lg" placeholder="Title" value="{{ old('title') }}"></textarea>
+                <textarea name="title" class="form-control @error('title') is-invalid @enderror" aria-label="With textarea" aria-describedby="inputGroup-sizing-lg" placeholder="Title">{{ old('title') }}</textarea>
+                @error('title')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
             </div>
             <div class="input-group mb-3">
-                <input name="file" type="file" class="form-control" id="chooseFile" aria-describedby="inputGroupFileAddon04" aria-label="Upload">
+                <input name="file" type="file" class="form-control @error('file') is-invalid @enderror" id="chooseFile" aria-describedby="inputGroupFileAddon04" aria-label="Upload">
                 <button class="btn btn-primary" type="button" id="inputGroupFileAddon04">Button</button>
+                @error('file')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                 @enderror
             </div>
             <div class="form-group mb-3">
-                <textarea name="content" class="form-control" aria-label="With textarea" aria-describedby="inputGroup-sizing-lg" placeholder="Content" value="{{ old('content') }}"></textarea>
+                <textarea name="content" class="form-control @error('content') is-invalid @enderror" aria-label="With textarea" aria-describedby="inputGroup-sizing-lg" placeholder="Content">{{ old('content') }}</textarea>
+                @error('content')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
             </div>
         </div>
         <div class="my-3 p-3 bg-white rounded shadow-sm">
@@ -28,7 +43,12 @@
                 <h4>Enter tags using the # key followed by the tag i.e., #PokemonGo #Event #News.
             </div>
         <div class="form-group mb-3">
-            <textarea name="tags" class="form-control" aria-label="With textarea" aria-describedby="inputGroup-sizing-lg" placeholder="Tags" value="{{ old('tags') }}"></textarea>
+            <textarea name="tags" class="form-control @error('tags') is-invalid @enderror" aria-label="With textarea" aria-describedby="inputGroup-sizing-lg" placeholder="Tags">{{ old('tags') }}</textarea>
+            @error('tags')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
         </div>
         <input class="btn btn-primary" type="submit" value="Submit Post">
         <a href="{{ route('posts.index') }}">Cancel</a>
