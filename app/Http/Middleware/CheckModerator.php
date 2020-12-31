e510438394a0d08d1ea7da8ad008d4bab4c8de06
Samuel Netherway <samuelnetherwaygit@gmail.com>
2020-12-31 00:13:22 +0000

    [POK-62/POK-160] Added functionality to check if the signed in user is moderator.

--- app/Http/Middleware/CheckModerator.php
+++ app/Http/Middleware/CheckModerator.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Auth;
 
 class CheckModerator
 {
@@ -15,6 +16,10 @@ class CheckModerator
      */
     public function handle($request, Closure $next)
     {
-        return $next($request);
+        if (Auth::user()->profile->profileable_type == App\Moderator::class) {
+            return $next($request);
+        } else {
+            return redirect('/posts')->with('warning', 'You cannot perform this action! You are not a moderator.');
+        }
     }
 }
